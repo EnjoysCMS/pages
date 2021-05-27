0eb2292a4344ad0093138dbf4b9d34f55d457aef
Enjoys <deadly.info@gmail.com>
2021-05-27 15:47:36 +0300

    _title html

--- src/Controller/Item.php
+++ src/Controller/Item.php
@@ -8,6 +8,7 @@ use App\Module\Pages\Entities\Page;
 use Doctrine\ORM\EntityManager;
 use Enjoys\Http\ServerRequestInterface;
 use EnjoysCMS\Core\Components\Helpers\Error;
+use EnjoysCMS\Core\Components\Helpers\Setting;
 use Twig\Environment;
 
 class Item
@@ -36,6 +37,7 @@ class Item
 
     public function view()
     {
+        /** @var Page $page */
         $page = $this->pagesRepository->findOneBy(['slug' => $this->serverRequest->get('slug'), 'status' => true]);
         if ($page === null) {
             Error::code(404);
@@ -44,12 +46,17 @@ class Item
         $template_path = '@m/pages/view.twig';
 
         if (!$this->twig->getLoader()->exists($template_path)) {
-            $template_path =  __DIR__ . '/../template/view.twig.sample';
+            $template_path = __DIR__ . '/../template/view.twig.sample';
         }
 
         return $this->twig->render(
             $template_path,
             [
+                '_title' => sprintf(
+                    '%2$s - %1$s',
+                    Setting::get('sitename'),
+                    $page->getTitle()
+                ),
                 'page' => $page
             ]
         );
