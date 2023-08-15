df63a559a2e10ae3e39caf61c24aaa4f543f4c26
enjoys <deadly.info@gmail.com>
2023-08-15 10:25:12 +0300

    refactor AbstractModuleConfig.php

--- src/Config.php
+++ src/Config.php
@@ -5,53 +5,15 @@ declare(strict_types=1);
 namespace EnjoysCMS\Module\Pages;
 
 
-use EnjoysCMS\Core\Modules\ModuleCollection;
-use InvalidArgumentException;
-use Symfony\Component\Yaml\Yaml;
+use EnjoysCMS\Core\Modules\AbstractModuleConfig;
 
-final class Config
+final class Config extends AbstractModuleConfig
 {
 
-    private const MODULE_NAME = 'enjoyscms/pages';
-
-    public function __construct(
-        private readonly \Enjoys\Config\Config $config,
-        ModuleCollection $moduleCollection
-    ) {
-        $module = $moduleCollection->find(self::MODULE_NAME) ?? throw new InvalidArgumentException(
-            sprintf(
-                'Module %s not found. Name must be same like packageName in module composer.json',
-                self::MODULE_NAME
-            )
-        );
-
-
-        if (file_exists($module->path . '/config.yml')) {
-            $config->addConfig(
-                [
-                    self::MODULE_NAME => file_get_contents($module->path . '/config.yml')
-                ],
-                ['flags' => Yaml::PARSE_CONSTANT],
-                \Enjoys\Config\Config::YAML,
-                false
-            );
-        }
-    }
-
-    public function get(string $key = null, mixed $default = null): mixed
+    public function getModulePackageName(): string
     {
-        if ($key === null) {
-            return $this->config->get(self::MODULE_NAME);
-        }
-        return $this->config->get(sprintf('%s->%s', self::MODULE_NAME, $key), $default);
+        return 'enjoyscms/pages';
     }
-
-
-    public function all(): array
-    {
-        return $this->config->get();
-    }
-
 
     public function getCrudContentEditor(): null|string|array
     {
